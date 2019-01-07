98d17de0e66f75e94d25dd90e1e99c84f5a45b52
Matt Hickman <matthickman@HickeyBook-Pro.local>
2019-01-06 18:15:14 -0800

    Added new pics

--- js/wallpapers.js
+++ js/wallpapers.js
@@ -17,10 +17,10 @@ document.getElementById("one").onclick = function() {
   var c = document.getElementById("myCanvas");
   var ctx = c.getContext("2d");
   var img = document.getElementById("one");
-  //ctx.drawImage(img, 0, 0, 324, 576);
-  ctx.font = "18px Dancing Script";
-  ctx.fillStyle = "white";
-  ctx.textAlign = "right";
+  ctx.drawImage(img, 0, 0, 324, 576);
+  //ctx.font = "18px Dancing Script";
+  //ctx.fillStyle = "white";
+  //ctx.textAlign = "right";
   //ctx.fillText(firstName, 316, 566);
   aye.href = "/images/wallpapers/CapeTownWallpaper.png";
   console.log("href changed to /images/wallpapers/CapeTownWallpaper.png");
